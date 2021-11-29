60d3e5f8abed36fe73e3487b0611db6959f98a06
Thad <thadisfree@gmail.com>
2021-11-29 10:14:30 -0600

    initial commit

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public void ListOut()
+        {
+
+        }
+    
+    
+    
+    
+    }
+
+
+    
+       
+
+        
+}
+
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Make = "Chevy";
+            car1.Model = "Nova";
+            car1.Year = 1974;
+
+            var car2 = new Car();
+            car2.Make = "Toyota";
+            car2.Model = "Sienna";
+            car2.Year = 2015;
+
+            var car3 = new Car();
+            car3.Make = "Kia";
+            car3.Model = "Forza";
+            car3.Year = 2018;
+
+            var carList = new List<Car>();
+            carList.Add(car1);
+            carList.Add(car2);
+            carList.Add(car3);
+
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+
+        }
+    }
+}
